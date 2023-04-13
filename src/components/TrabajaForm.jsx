b5c45e1533a5f19af3b876029a73ddc349df83ec
jans027 <jans027@gmail.com>
2023-04-13 13:10:03 -0500

    resolbiendo bug btn pdf

--- src/components/TrabajaForm.jsx
+++ src/components/TrabajaForm.jsx
@@ -170,7 +170,7 @@ const TrabajaForm = (props) => {
             // console.log(event.target.value)
 
             const url = SERVICE_URL;
-            const Nombre = 'TRABAJA';
+            const Nombre = 'CURRICULUM';
             //we get consecutive number to generate a different conversation email each time
             const messageId = uuidv4().slice(0, 8);
 
@@ -198,44 +198,50 @@ const TrabajaForm = (props) => {
                     `
                 })
             })
-            .then(response => {
-                if (response.ok) {
-                    // throw new Error('La petición ha fallado!')
-                    return response.json()
-                        .then(data =>
-                            // console.log(data),
-                            formRef.current.reset(),// clean form
-                            toast.success('En breve nos pondremos en contacto contigo!', {// alert message
-                                position: "top-center",
-                                autoClose: 1000,
-                                hideProgressBar: false,
-                                closeOnClick: true,
-                                pauseOnHover: true,
-                                draggable: true,
-                                progress: undefined,
-                                theme: "light",
-                            })
+                .then(response => {
+                    setProgress(false);
+                    if (response.ok) {
+                        // throw new Error('La petición ha fallado!')
+                        return response.json()
+                            .then(data =>
+                                // console.log(data),
+                                setProgress(false),
+                                setLabelFile(''),
+                                formRef.current.reset(),// clean form
+                                toast.success('En breve nos pondremos en contacto contigo!', {// alert message
+                                    position: "top-center",
+                                    autoClose: 1000,
+                                    hideProgressBar: false,
+                                    closeOnClick: true,
+                                    pauseOnHover: true,
+                                    draggable: true,
+                                    progress: undefined,
+                                    theme: "light",
+                                })
                             )
-                        .catch(error => console.error(error));
-                } else {
-                    // throw new Error('La petición ha fallado!')
-                    console.log(response.status)
-                    toast.error('No se pudo Enviar tu solicitud, Intentalo mas tarde!', {// alert message
-                        position: "top-center",
-                        autoClose: 1000,
-                        hideProgressBar: false,
-                        closeOnClick: true,
-                        pauseOnHover: true,
-                        draggable: true,
-                        progress: undefined,
-                        theme: "light",
-                    })
-                }
-            })
-            .catch(error =>
-                console.log(error),
-                formRef.current.reset(),// clean form
-            );
+                            .catch(error => console.error(error));
+                    } else {
+                        // throw new Error('La petición ha fallado!')
+                        console.log(response.status)
+                        setProgress(false)
+                        setLabelFile('')
+                        formRef.current.reset()// clean form
+                        toast.error('No se pudo Enviar tu solicitud, Intentalo mas tarde!', {// alert message
+                            position: "top-center",
+                            autoClose: 1000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            pauseOnHover: true,
+                            draggable: true,
+                            progress: undefined,
+                            theme: "light",
+                        })
+                    }
+                })
+                .catch(error =>
+                    console.log(error),
+                    formRef.current.reset(),// clean form
+                );
         }
 
     };
